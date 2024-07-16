b4708d2dfcf1203a08f76d69a19985899390b3fe
Snayff <snaith2@gmail.com>
2024-07-16 15:53:24 +0100

    end part 7 - enemy, hitbox & hurtbox

--- enemies/enemy.gd
+++ enemies/enemy.gd
@@ -1,12 +1,17 @@
 extends Node2D
+
+@onready var stats_component: StatsComponent = $StatsComponent
+@onready var move_component: MoveComponent = $MoveComponent
 @onready var visible_on_screen_notifier_2d: VisibleOnScreenNotifier2D = $VisibleOnScreenNotifier2D
 @onready var scale_component: ScaleComponent = $ScaleComponent
 @onready var flash_component: FlashComponent = $FlashComponent
+@onready var shake_component: ShakeComponent = $ShakeComponent
+@onready var hurtbox_component: HurtboxComponent = $HurtboxComponent
 @onready var hitbox_component: HitboxComponent = $HitboxComponent
 
 
 func _ready() -> void:
-	scale_component.tween_scale()
-	flash_component.flash()
 	visible_on_screen_notifier_2d.screen_exited.connect(queue_free)
-	hitbox_component.hit_hurtbox.connect(queue_free.unbind(1))  # ignore the arg passed by the signal
+	hurtbox_component.hurt.connect(func(hitbox: HitboxComponent):
+		queue_free()
+	)
