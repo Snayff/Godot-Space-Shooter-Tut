05e565c70bb471c2557024466dbe8d180bef2e46
Snayff <snaith2@gmail.com>
2024-07-23 14:12:09 +0100

    end part 9 - actor destroyed

--- enemies/enemy.gd
+++ enemies/enemy.gd
@@ -8,10 +8,22 @@ extends Node2D
 @onready var shake_component: ShakeComponent = $ShakeComponent
 @onready var hurtbox_component: HurtboxComponent = $HurtboxComponent
 @onready var hitbox_component: HitboxComponent = $HitboxComponent
+@onready var destroyed_component: DestroyedComponent = $DestroyedComponent
 
 
 func _ready() -> void:
 	visible_on_screen_notifier_2d.screen_exited.connect(queue_free)
-	hurtbox_component.hurt.connect(func(hitbox: HitboxComponent):
-		queue_free()
+	hurtbox_component.hurt.connect(#receive_hit
+# previously used a lambda, but pycharm gave error (godot didnt)
+		func(hitbox: HitboxComponent):
+			scale_component.tween_scale()
+			flash_component.flash()
+			shake_component.tween_shake()
 	)
+	stats_component.no_health.connect(queue_free)
+	hitbox_component.hit_hurtbox.connect(destroyed_component.destroy.unbind(1))
+
+func receive_hit(hitbox: HitboxComponent) -> void:
+	scale_component.tween_scale()
+	flash_component.flash()
+	shake_component.tween_shake()
